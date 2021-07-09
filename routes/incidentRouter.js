f584b8183a79351edd957361e88d11979d4b91f3
Jonathan Cardenas <jonscardenas@gmail.com>
2021-07-08 23:00:16 -0500

    finished all end points

--- routes/incidentRouter.js
+++ routes/incidentRouter.js
@@ -39,6 +39,8 @@ incidentRouter.route('/')
 });
 
 
+
+
 incidentRouter.route('/:incidentId')
 .get((req, res, next) => {
     Incident.findById(req.params.incidentId)
@@ -74,6 +76,11 @@ incidentRouter.route('/:incidentId')
     .catch(err => next(err));
 });
 
+
+
+/*
+For Items
+*/
 
 incidentRouter.route('/:incidentId/items')
 .get((req, res, next) => {
@@ -138,6 +145,100 @@ incidentRouter.route('/:incidentId/items')
     })
     .catch(err => next(err));
 });
+
+
+
+incidentRouter.route('/:incidentId/items/:itemId')
+    .get((req, res, next) => {
+        Incident.findById(req.params.incidentId)
+        //.populate('comments.author')
+        .then(incident => {
+            if (incident && incident.items.id(req.params.itemId)) {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(incident.items.id(req.params.itemId));
+            } else if (!incident) {
+                err = new Error(`Incident ${req.params.incidentId} not found`);
+                err.status = 404;
+                return next(err);
+            } else{
+                err = new Error(`Item ${req.params.itemId} not found`);
+                err.status = 404;
+                return next(err);
+            }
+        })
+        .catch(err => next(err));
+    })
+    .post((req, res) => {
+        res.statusCode = 403;
+        res.end(`POST operation not supported on /incidents/${req.params.incidentId}/items/${req.params.itemId}`);
+    })
+    .put((req, res, next) => {
+        Incident.findById(req.params.incidentId)
+        //.populate('comments.author')
+        .then(incident => {
+
+            if (incident && incident.items.id(req.params.itemId)) {
+                if (req.body.type){
+                    incident.items.id(req.params.itemId).type = req.body.type;
+                }
+
+                if (req.body.locationFound){
+                    incident.items.id(req.params.itemId).locationFound = req.body.locationFound;
+                }
+
+                if (req.body.description){
+                    incident.items.id(req.params.itemId).description = req.body.description;
+                }
+
+                if (req.body.date){
+                    incident.items.id(req.params.itemId).date = req.body.date;
+                }
+
+                incident.save()
+                .then(incident => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(incident);
+                })
+                .catch(err => next(err));
+            } else if (!incident) {
+                err = new Error(`Incident ${req.params.incidentId} not found`);
+                err.status = 404;
+                return next(err);
+            } else{
+                err = new Error(`Item ${req.params.itemId} not found`);
+                err.status = 404;
+                return next(err);
+            }
+        })
+        .catch(err => next(err));        
+    })
+    .delete((req, res, next) => {
+        //deletes every document in collection
+        Incident.findById(req.params.incidentId)
+        .then(incident => {
+            if (incident && incident.items.id(req.params.itemId)) {
+                incident.items.id(req.params.itemId).remove();
+                incident.save()
+                .then(incident => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(incident);
+                })
+                .catch(err => next(err));
+            } else if (!incident) {
+                err = new Error(`Incident ${req.params.incidentId} not found`);
+                err.status = 404;
+                return next(err);
+            } else{
+                err = new Error(`Item ${req.params.itemId} not found`);
+                err.status = 404;
+                return next(err);
+            }
+        })
+        .catch(err => next(err));
+    });
 
 
 module.exports = incidentRouter;
